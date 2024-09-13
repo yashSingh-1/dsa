af8f02655f2033605b43041c92829d6238d422ec
Yash Singh <dobbyneedsclothes@gmail.com>
2024-09-13 18:31:08 +0530

    First

---
+++ natural.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class natural {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        natural demo = new natural();
+        int num = sc.nextInt();
+        // int sum = 0;
+        // for (int i = 1; i <= num; i++ ){
+            //     sum += i;
+            // }
+        double now = System.currentTimeMillis();
+        System.out.println("The time rn is :" + (now));
+        int sum = demo.findSum(num);
+        System.out.println("Time taken in ms: " + (System.currentTimeMillis() - now) + "milliseconds");
+        System.out.println("Here is the sum :" + (sum));
+        sc.close();
+        demo.pattern();
+    }
+
+    public int findSum(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    public void pattern() {
+        for (int i = 1; i<=5; i++){
+            for(int j = 5; j >= i; j-- ) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
