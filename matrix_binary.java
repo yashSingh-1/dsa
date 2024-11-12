8a69e6175bbdf65f028ce7f775bef3106ead0f86
Yash Singh <dobbyneedsclothes@gmail.com>
2024-11-12 12:51:23 +0530

    2d Binary retry

--- matrix_binary.java
+++ matrix_binary.java
@@ -7,14 +7,21 @@ public class matrix_binary {
             {23, 35, 37},
             {38, 40, 49}
         };
+        
+        // int[] values = search(arr, 40);
+        // System.out.println(Arrays.toString(values));
 
-        int[] values = search(arr, 49);
-        System.out.println(Arrays.toString(values));
+
+        int[] reevaluate = try_serach(arr, 41);
+        System.out.println(Arrays.toString(reevaluate));
+
     }
 
     static int[] search(int[][] arr, int target){
         int r = 0;
         int c = arr.length - 1;
+        System.out.println("Row: " + r + " Col: " + c);
+
 
         while (r < arr.length && c >= 0) {
             if(arr[r][c] == target){
@@ -32,5 +39,24 @@ public class matrix_binary {
         }
 
         return new int[]{-1, -1};
+    }
+
+    static int[] try_serach(int[][] arr, int target){
+        int row = 0;
+        int col = arr.length - 1;
+
+        while(row < arr.length && col >= 0){
+            if(target == arr[row][col]){
+                return new int[] {row, col};
+            }
+
+            if(arr[row][col] < target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+
+        return new int[] {-1, -1};
     }
 }
