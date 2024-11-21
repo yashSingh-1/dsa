2d7e57062eb150dbd245f941d58e6e2c8d269bbd
Yash Singh <dobbyneedsclothes@gmail.com>
2024-11-21 12:05:08 +0530

    bubbleSort

--- bubble.java
+++ bubble.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class bubble {
     public static void main(String[] args) {
         int[] arr = {
-            5, 4, 3, 2, 1
+            5, 5, 5, 5, 5
         };
 
         bubbleSort(arr);
@@ -11,15 +11,20 @@ public class bubble {
     }
 
     static void bubbleSort(int[] arr){
-
+        boolean swap = false;
         for(int i = 0; i < arr.length; i++){
             for(int j = 1; j < arr.length - i; j++){
+                swap = false;
                 System.out.println("I : " + i + " J: " + j);
                 if(arr[j] < arr[j - 1]){
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    swap = true;
                 }
+            }
+            if(!swap){
+                break;
             }
         }
     }
