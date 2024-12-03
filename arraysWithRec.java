9a4f2a0929e9bc0f6318b6c7fb6d1ecac112252d
Yash Singh <dobbyneedsclothes@gmail.com>
2024-12-03 17:33:42 +0530

    Wrong rotatedBinary brute

--- arraysWithRec.java
+++ arraysWithRec.java
@@ -1,11 +1,15 @@
 public class arraysWithRec {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,6,5};
+        // int[] arr = {1,2,3,4,6,5};
         // boolean isIt = sort(arr, 0);
         // System.out.println(isIt);
 
-        int index = LinearSearch(arr, 0, 6);
-        System.out.println(index);
+        // int index = LinearSearch(arr, 0, 6);
+        // System.out.println(index);
+
+        int[] newArr = {5,6,7,8,9,1,2,3};
+        int found = RotatedBinary(newArr, 2);
+        System.out.println(found);
     }
 
     static boolean sort(int[] arr, int s ){
@@ -27,4 +31,49 @@ public class arraysWithRec {
 
         return LinearSearch(arr, start + 1, target);
     }
+
+    static int RotatedBinary(int[] arr, int target){
+        int found = -1;
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = start + (end - start) / 2;
+        if(arr[0] <= arr[mid] && target >= arr[0] && target <= arr[mid] ){
+            System.out.println("I the 1st half");
+
+                end = mid;
+                while(start <= end){
+                    mid = start + (end - start) / 2;
+                    if(target > arr[mid]){
+                        start = mid + 1;
+                    }else if(target < arr[mid]){
+                        end = mid - 1;
+                    }else {
+                        found = mid;
+                        return found;
+                    }
+                }
+            }
+
+        if(arr[mid + 1] <= arr[end] && target >= arr[mid] && target <= arr[end]){
+            System.out.println("I the 2nd half");
+
+                start = mid;
+                while(start <= end){
+                    mid = start + (end - start) / 2;
+                    if(target > arr[mid]){
+                        start = mid + 1;
+                    }else if(target < arr[mid]){
+                        end = mid - 1;
+                    }else {
+                        found = mid;
+                        return found;
+                    }
+                }
+            }
+
+        System.out.println("start: " + start + " end : " + end + " mid: " + mid);
+
+        return found;
+    }
 }
+
--- intersection.java
+++ intersection.java
@@ -1,23 +1,27 @@
+import java.util.ArrayList;
+
 public class intersection {
     public static void main(String[] args) {
-        int[] arr1 = {1,2,3,45,6,7};
+        int[] arr1 = {1,2,3,45,6,7,6,22,31};
         int[] arr2 = {5,6,6,7,22,31};
 
-        int num = checkIntersection(arr1, arr2);
+        ArrayList<Integer> num = checkIntersection(arr1, arr2);
         System.out.println(num);
         
     }
 
     // O(n^2) time..this not good enough 
-    static int checkIntersection(int[] arr1, int[] arr2){
-        int common = 0;
+    static ArrayList<Integer> common = new ArrayList<>();
+    static ArrayList<Integer> checkIntersection(int[] arr1, int[] arr2){
+        // int common = 0;
         for(int i = 0; i < arr1.length; i++){
             for(int j = 0; j < arr2.length; j++){
                 if(arr1[i] == arr2[j]){
-                    common = arr1[i];
-                    return common;
-                }else {
-                    common = -1;
+                    if(common.contains(arr1[i])){
+                        j++;
+                    }else{
+                        common.add(arr1[i]);
+                    }
                 }
             }
         }
